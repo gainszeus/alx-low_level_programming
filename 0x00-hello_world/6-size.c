8f4e5f24228d91414c0ef43fbee8a012bcc98b55
gainszeus <gainszeus4112@gmail.com>
2022-12-10 04:26:24 -0800

    no territory can make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 int main(void)
 	/*the code starts here*/
 {
-	printf("size of a char:%d byte(s)\n", sizeof(char));
-	printf("size of an int:%d byte(s)\n", sizeof(short int));
-	printf("size of a long int:%d byte(s)\n", sizeof(long int));
-	printf("size of a long long int:%d byte(s)\n", sizeof(long long int));
-	printf("size of a float:%d byte(s)\n", sizeof(float));
+	printf("size of a char:%lu byte(s)\n", sizeof(char));
+	printf("size of an int:%lu byte(s)\n", sizeof(short int));
+	printf("size of a long int:%lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int:%lu byte(s)\n", sizeof(long long int));
+	printf("size of a float:%lu byte(s)\n", sizeof(float));
 return (0);
 }
 
