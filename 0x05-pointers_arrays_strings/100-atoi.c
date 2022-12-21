4aeb3e47cb9163cbada19326029e4be151b6c50f
gainszeus <gainszeus4112@gmail.com>
2022-12-20 18:18:55 -0800

    convert a string to an int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - a function that converts a string to an int
+ * @s: the string to be converted
+ * Return: the int value of the string converted
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
