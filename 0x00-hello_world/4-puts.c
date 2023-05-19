de26dabf03c70556fecb272c0634c1a1b604f360
gainszeus <gainszeus4112@gmail.com>
2023-05-18 19:19:56 -0700

    first c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main point
+ * return : always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzel");
+	return (0);
+}
